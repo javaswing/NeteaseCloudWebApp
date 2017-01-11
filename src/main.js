b5050366868e054f35b7d60226163f9bb2d09e79
javaSwing <zxdlovejava@gmail.com>
2017-01-11 22:01:29 +0800

    First Commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
