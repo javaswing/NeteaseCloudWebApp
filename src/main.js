3ceb90e30b3920a1746023834b9036166a7b656e
张晓东 <zxdlovejava@gmail.com>
2017-02-27 22:49:18 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import Axios from './utils/diyaxios'
-import VueLazyload from 'vue-lazyload'
+// import VueLazyload from 'vue-lazyload'
 import router from './router/router'
 import store from './store'
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
 import 'assets/theme.less'
 Vue.use(MuseUI)
-Vue.use(VueLazyload)
+// Vue.use(VueLazyload)
 Vue.prototype.$http = Axios // 类似于vue-resource的调用方法，之后可以在实例里直接用this.$http.get()等
 
 /* eslint-disable no-new */
